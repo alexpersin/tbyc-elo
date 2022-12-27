0ca117a93ead5d26d06d06c302bc0d0f24e2b74c
Alex Persin <apersin11@gmail.com>
2022-12-27 10:25:48 +0000

    change K to 80

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # parameters for the Elo algorithm -- setting kind of arbitrarily for now, should tune once we have more data
-DEFAULT_K_VALUE = 50
+DEFAULT_K_VALUE = 80
 DEFAULT_D_VALUE = 600
 DEFAULT_SCORING_FUNCTION_BASE = 1.0
 INITIAL_RATING = 1000
